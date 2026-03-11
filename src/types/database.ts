4526235b96bf25d949c42a0a4302249a76540ae0
AniketW42 <aniketwakte42@gmail.com>
2026-03-11 23:05:21 +0530

    fix: workflow version schema

--- src/types/database.ts
+++ src/types/database.ts
@@ -745,7 +745,7 @@ export interface WorkflowVersion {
   modified_on: Generated<Timestamp>;
   published_on: Timestamp | null;
   status: WorkflowVersionStatus;
-  version: Generated<number>;
+  version: number;
   workflow_id: string;
 }
 
